eda82a64acadcd152d72b432c92b7d0664140a16
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-05-31 02:20:14 +0600

    add to cart and save to db

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 function FoodCard({ items }) {
-    const { name, image, recipe, price } = items;
+    const { user } = useContext(AuthContext);
+    const { name, image, recipe, price, _id } = items;
+    const navigate = useNavigate();
+    const location = useLocation();
+
     const handleAddToCart = (item) => {
-        console.log(item);
+        if (user && user.email) {
+            const cartItem = { itemId: _id, name, image, price, email: user.email };
+            fetch('http://localhost:5000/carts', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(cartItem),
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Add To Cart Successfully',
+                            showConfirmButton: false,
+                            timer: 1500,
+                        });
+                    }
+                });
+        } else {
+            Swal.fire({
+                title: 'Are you sure?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Fast',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } });
+                }
+            });
+        }
     };
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
